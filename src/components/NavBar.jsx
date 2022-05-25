524e6916d776e75ef8b0eccc71fb09fab681f1e2
Leslie Herrera <lherrera@ecomsur.com>
2022-05-24 23:29:45 -0400

    clean app and nav added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Logo</li>
+                <li>Menu</li>
+                <li>Info</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
