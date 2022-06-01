0208edb82c2d045db5eff556f28c004e7f2d0831
Leslie Herrera <lherrera@ecomsur.com>
2022-05-31 22:53:14 -0400

    añadidos hooks y contador

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+
+const ItemCount = ({ initial, stock }) => {
+
+    const [count, setCount] = useState(parseInt(initial))
+
+    const handleSub = () => {
+        setCount(parseInt(count - 1))
+    }
+    const handleSum = () => {
+        setCount(parseInt(count + 1))
+    }
+
+    useEffect(() => {
+        if (count > parseInt(stock)) {
+            console.log("mayor a 5")
+        } else if (count <= 0) {
+            console.log("menor o igual a 0")
+        }
+    }, [count, stock])
+
+    return (
+        <>
+            <div className="cardComponent">
+                <div className="imgCard">
+                    <img src="https://via.placeholder.com/150" alt="" />
+                </div>
+                <h3>Product Name</h3>
+                <div className="counterCard">
+                    <span>
+                        <button onClick={handleSub} ><FontAwesomeIcon icon={faMinus} /></button>                    </span>
+                    <span>{count}</span>
+                    <span>
+                        <button onClick={handleSum}><FontAwesomeIcon icon={faPlus} /></button>
+                    </span>
+                </div>
+                <div className="addToCart">Add to Cart <FontAwesomeIcon icon={faShoppingCart} /></div>
+            </div>
+        </>
+    );
+}
+
+export default ItemCount; 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,16 @@
-const ItemListContainer = (props) => {
-    return ( <>
-        <h1>Listado de Productos</h1>
-        <h1>Hey, Wellcome {props.person}</h1>
-        <img className="mainImage" src="../img/mirror-woman.jpg" alt="mirror woman" />
+import ItemCount from './ItemCount';
 
-    </> );
+const ItemListContainer = (props) => {
+    return (
+        <>
+            <h1>Listado de Productos</h1>
+            <h1>Hey, Wellcome {props.person}</h1>
+
+            <section>
+                <ItemCount stock="5" initial="1" />
+            </section>
+        </>
+    );
 }
- 
+
 export default ItemListContainer;
